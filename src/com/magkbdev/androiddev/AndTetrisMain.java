78e50e26f3ba612a35999d4879aeac5ae24273b0
stevegocoding <stevegocoding@gmail.com>
2012-12-04 16:44:57 -0800

    Initial commit

---
+++ src/com/magkbdev/androiddev/AndTetrisMain.java
@@ -0,0 +1,21 @@
+package com.magkbdev.androiddev;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+/**
+ * 
+ * @author Guangfu Shi(Steve) 
+ *
+ */
+public class AndTetrisMain extends SimpleBaseGameActivity
+{
+	
+	public EngineOptions onCreateOptions()
+	{
+		
+	}
+
+}
